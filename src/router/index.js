57291373cd250c00b83c73b70524e09fbf77975b
punmagarsumit-nwsl <s-punmagar@new-wave.jp>
2025-03-07 16:41:04 +0900

    undo og

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
-import GuideView from '../views/GuideView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,6 @@ const router = createRouter({
             name: 'main',
             component: MainView
         },
-        {
-            path: '/guide',
-            name: 'guide',
-            component: GuideView,
-          },
     ],
     scrollBehavior(to, from, savedPosition) {
         if (to.hash && to.hash == '#landing-page') {
